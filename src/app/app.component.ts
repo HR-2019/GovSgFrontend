f29aa09a83db8cfb1064ebd960e916685ae77de0
HR-2019 <56949560+HR-2019@users.noreply.github.com>
2022-12-20 21:32:04 -0500

    Actualización de AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FiscalPositionDialogAddComponent } from './components/fiscal-positions/fiscal-position-dialog-add/fiscal-position-dialog-add.component';
+import { FiscalPositionService } from './services/fiscal-position.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'govsgfrontend';
+
+  constructor(public dialog: MatDialog, private fiscalPositionService: FiscalPositionService){
+
+  }
+
+  openDialog() {
+    this.dialog.open(FiscalPositionDialogAddComponent, {
+      width: '30%'
+    }).afterClosed().subscribe(
+      val => {
+        console.log(val);
+        if (val === 'save'){
+          this.fiscalPositionService.getAllFiscalPositions('id', 'asc', 0);
+        }
+      }
+    )
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,52 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FiscalPositionListComponent } from './components/fiscal-positions/fiscal-position-list/fiscal-position-list/fiscal-position-list.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSortModule } from '@angular/material/sort';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { FiscalPositionDialogAddComponent } from './components/fiscal-positions/fiscal-position-dialog-add/fiscal-position-dialog-add.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
-    FiscalPositionListComponent
+    FiscalPositionListComponent,
+    FiscalPositionDialogAddComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
